d924f0a4b3ab15c01ae322ce32b1c9e91619d843
Crisandro Basoy <Crisandro.Basoy@cebupacificair.com>
2026-03-19 12:10:52 +0800

    add session and local storage implementation

---
+++ src/app/services/sessionStorage.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from "@angular/core";
+import { Observable, Subject } from "rxjs";
+import { CryptoService } from "./crypto.service";
+import { SessionStorageKeys } from "../enums/sessionStorageKeys.enum";
+
+@Injectable({
+  providedIn: "root"
+})
+export class SessionStorageService {
+
+  private updated$ = new Subject<void>();
+
+  constructor(private cryptoService: CryptoService) {}
+
+  public setObjectItem<T>(key: string, value: T): void {
+    const encryptedValue = this.cryptoService.encrypt(value);
+    sessionStorage.setItem(key, encryptedValue);
+    this.updated$.next();
+  }
+
+  public getObjectItem<T>(key: string): T | null {
+    const encryptedValue = sessionStorage.getItem(key);
+
+    if (!encryptedValue) return null;
+
+    return this.cryptoService.decrypt<T>(encryptedValue);
+  }
+
+  public remove(key: string): void {
+    sessionStorage.removeItem(key);
+    this.updated$.next();
+  }
+
+  public removeSessionStorages(keys: Array<SessionStorageKeys>): void {
+    keys.forEach((key) => this.remove(key));
+  }
+
+  public get valueChanges(): Observable<void> {
+    return this.updated$.asObservable();
+  }
+}
